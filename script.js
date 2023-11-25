46c0a7ac3ab8cedd8aaf74dd0861c128b602aefe
Didier <115608922+DogGotSwag@users.noreply.github.com>
2023-11-25 00:11:21 -0700

    Add container of grid put it in the center

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello");
+let container = document.createElement("div");
+container.style.cssText = "border: solid 2px black";
+
+let html = document.querySelector('html');
+html.style.cssText = "height: 100%;";
+let body = document.querySelector("body");
+
+body.style.cssText = "height: 100%; display: flex; align-items: center; justify-content: center;"
+body.appendChild(container);
