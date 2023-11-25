75a880cf76a2cbd585d298d1157ef12293134159
Didier <115608922+DogGotSwag@users.noreply.github.com>
2023-11-25 01:19:58 -0700

    make creatGrid function
    now that is called at the start of the program with the
    input of 16 and it can be called again with diffrent numbers
    and adjust the width and height of divs accordingly

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-let fragment = new DocumentFragment();
+function createGrid( numberOfSquaresPerSide ){
+    let fragment = new DocumentFragment();
 
-let container = document.createElement("div");
-container.style.cssText = "border: solid 2px black; width: 480px; height: 480px; display: flex; flex-wrap: wrap;";
+    let container = document.createElement("div");
+    container.style.cssText = "border: solid 2px black; width: 480px; height: 480px; display: flex; flex-wrap: wrap;";
 
-let html = document.querySelector('html');
-html.style.cssText = "height: 100%;";
-let body = document.querySelector("body");
+    let html = document.querySelector('html');
+    html.style.cssText = "height: 100%;";
+    let body = document.querySelector("body");
 
-body.style.cssText = "height: 100%; display: flex; align-items: center; justify-content: center; flex-direction: column;"
-fragment.appendChild(container);
+    body.style.cssText = "height: 100%; display: flex; align-items: center; justify-content: center; flex-direction: column;"
+    fragment.appendChild(container);
 
 
 
-for(let i = 0; i < 256; i++){
-    let gridBox = document.createElement('div');
-    gridBox.style.cssText = 'box-sizing: border-box; width: 30px; height: 30px; border: 1px solid black;';
+    for(let i = 0; i < numberOfSquaresPerSide*numberOfSquaresPerSide; i++){
+        let pixelsPerSquare = 480/numberOfSquaresPerSide;
+        let gridBox = document.createElement('div');
+        gridBox.style.cssText = `box-sizing: border-box; width: ${pixelsPerSquare}px; height: ${pixelsPerSquare}px; border: 1px solid black;`;
 
-    gridBox.addEventListener('mouseover', () =>{
-        gridBox.style.cssText += "background-color: black;";
-    })
-    container.appendChild(gridBox);
+        gridBox.addEventListener('mouseover', () =>{
+            gridBox.style.cssText += "background-color: black;";
+        })
+        container.appendChild(gridBox);
+    }
+    
+    return fragment;
 }
+
+let body = document.querySelector('body');
 
 let button = document.createElement('button');
 button.textContent = "Change Size";
@@ -31,4 +38,4 @@ button.addEventListener('click', () =>{
 });
 
 body.appendChild(button);
-body.appendChild(fragment);
+body.appendChild( createGrid(16) );
