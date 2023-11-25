460c5e25e222f2a1665cf9ddc2f8b2be8447dcca
Didier <115608922+DogGotSwag@users.noreply.github.com>
2023-11-25 01:09:03 -0700

    Add button that generates prompt
    prompt will be used to change grid

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let html = document.querySelector('html');
 html.style.cssText = "height: 100%;";
 let body = document.querySelector("body");
 
-body.style.cssText = "height: 100%; display: flex; align-items: center; justify-content: center;"
+body.style.cssText = "height: 100%; display: flex; align-items: center; justify-content: center; flex-direction: column;"
 fragment.appendChild(container);
 
 
@@ -15,7 +15,20 @@ fragment.appendChild(container);
 for(let i = 0; i < 256; i++){
     let gridBox = document.createElement('div');
     gridBox.style.cssText = 'box-sizing: border-box; width: 30px; height: 30px; border: 1px solid black;';
+
+    gridBox.addEventListener('mouseover', () =>{
+        gridBox.style.cssText += "background-color: black;";
+    })
     container.appendChild(gridBox);
 }
 
+let button = document.createElement('button');
+button.textContent = "Change Size";
+button.style.cssText = 'margin-bottom: 100px;';
+
+button.addEventListener('click', () =>{
+    let newSize = prompt("How many sqaures per side?");
+});
+
+body.appendChild(button);
 body.appendChild(fragment);
