41b7d95a55f083fb21d400f1aa5b27f6046883ef
Didier <115608922+DogGotSwag@users.noreply.github.com>
2023-11-25 00:26:20 -0700

    add the square divs inside the grid

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+let fragment = new DocumentFragment();
+
 let container = document.createElement("div");
-container.style.cssText = "border: solid 2px black";
+container.style.cssText = "border: solid 2px black; width: 480px; height: 480px; display: flex; flex-wrap: wrap;";
 
 let html = document.querySelector('html');
 html.style.cssText = "height: 100%;";
 let body = document.querySelector("body");
 
 body.style.cssText = "height: 100%; display: flex; align-items: center; justify-content: center;"
-body.appendChild(container);
+fragment.appendChild(container);
+
+
+
+for(let i = 0; i < 256; i++){
+    let gridBox = document.createElement('div');
+    gridBox.style.cssText = 'box-sizing: border-box; width: 30px; height: 30px; border: 1px solid black;';
+    container.appendChild(gridBox);
+}
+
+body.appendChild(fragment);
